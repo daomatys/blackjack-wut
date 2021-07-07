e2d27315c9391eb97e1ec51bdcded28dbd2923cb
Maxim Dejnew <red41tiger@mail.ru>
2021-07-07 17:35:04 +0700

    Extracted hands class from Table to Deck

--- Deck/index.js
+++ Deck/index.js
@@ -3,47 +3,63 @@ export default class Deck {
   constructor() {
     this.elem = document.createElement('div');
     this.elem.classList.add('deck');
-    this.elem.insertAdjacentHTML('afterbegin', this.layout());
     
-    this.initStyle();
-    this.drawingCard();
+    this.elem.insertAdjacentHTML(
+      'afterbegin',
+      this.layoutHands() + 
+      this.layoutDeck()
+    );
+    this.defineDeckLocation();
+    this.drawTopCard();
   }
   
-  layout() {
+  layoutDeck() {
     return `
-      <div class="deck__veiled">
-        <img src="/assets/cards/back_red_deck.png" width="126px">
-      </div>
-      <div class="deck__top">
-        <img src="/assets/cards/back_red.png" width="120px">
+      <div class="deck">
+        <div class="deck__veiled">
+          <img src="/assets/cards/back_red_deck.png" width="126px">
+        </div>
+        <div class="deck__top">
+          <img src="/assets/cards/back_red.png" width="120px">
+        </div>
       </div>`;
   }
   
-  sub = suffix => this.elem.querySelector(`.deck__${ suffix }`);
-  
-  initStyle() {
-    const top =
-      document.documentElement.clientHeight / 2 - 
-      this.elem.style.height / 2;
-    
-    console.log(this.elem.offsetHeight)
-    this.sub('veiled').style.top = top + 'px';
-    this.sub('top').style.top = top - 1 + 'px';
+  layoutHands() {
+    return `
+      <div class="hand">
+        <div class="hand__robot"></div>
+        <div class="hand__playa">
+          <img src="/assets/cards/glowing_frame.png" width="120px">
+        </div>
+      </div>`;
   }
   
-  drawingCard() {
+  deckSub = suffix => this.elem.querySelector(`.deck__${ suffix }`);
+  
+  defineDeckLocation() {
+    const top = document.documentElement.clientHeight / 2 - 100;
+    
+    this.deckSub('veiled').style.top = top + 'px';
+    this.deckSub('top').style.top = top - 1 + 'px';
+  }
+  
+  drawTopCard() {
     this.elem.ondragstart = () => false;
     
-    this.sub('top').addEventListener('pointerdown', this.onPointerDown);
+    this.deckSub('top').addEventListener('pointerdown', this.onPointerDown);
   }
   
   onPointerDown = event => {
     event.preventDefault();
     
-    this.sub('top').style.opacity = 1;
+    this.deckSub('top').style.opacity = 1;
     
-    this.shiftX = event.clientX - this.sub('top').getBoundingClientRect().left;
-    this.shiftY = event.clientY - this.sub('top').getBoundingClientRect().top;
+    this.playahand = document.querySelector('.hand__playa')
+    this.playahand.style.opacity = 1;
+    
+    this.shiftX = event.clientX - this.deckSub('top').getBoundingClientRect().left;
+    this.shiftY = event.clientY - this.deckSub('top').getBoundingClientRect().top;
     
     document.addEventListener('pointermove', this.onPointerMove);
     document.addEventListener('pointerup', this.onPointerUp);
@@ -52,13 +68,15 @@ export default class Deck {
   onPointerMove = event => {
     event.preventDefault();
     
-    Object.assign( this.sub('top').style, {
+    Object.assign( this.deckSub('top').style, {
       top: event.pageY - this.shiftY + 'px',
       left: event.pageX - this.shiftX + 'px',
     });
   }
   
   onPointerUp = event => {
+    this.playahand.style.opacity = 0;
+    
     document.removeEventListener('pointermove', this.onPointerMove);
     document.removeEventListener('pointerup', this.onPointerUp);
   }
