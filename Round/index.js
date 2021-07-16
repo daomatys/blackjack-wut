b3d5578bc437b21f737d9dee442196c0164fd476
Maxim Dejnew <red41tiger@mail.ru>
2021-07-16 23:15:54 +0700

    Added dealer firstcard autodraw

--- Round/index.js
+++ Round/index.js
@@ -4,11 +4,12 @@ export default class Round {
   
   constructor() {
     this.deck = new Deck();
+    
     this.initRound();
   }
   
   initRound() {
-    //this.clearListeners();
+    this.clearListeners();
     
     this.dealerCardsCount = 0;
     this.dealerCardsValue = 0;
@@ -16,7 +17,9 @@ export default class Round {
     this.playerCardsCount = 0;
     this.playerCardsValue = 0;
     
-    this.eventListeners();
+    this.initListeners();
+    
+    this.firstAutodraw();
   }
   
   clearListeners() {
@@ -24,46 +27,66 @@ export default class Round {
       .elem
       .removeEventListener(
         'card-placed',
-        ({ detail: data }) => this.setNewDraggedCard( data )
+        ({ detail: data }) => this.setNewPlayerCard( data )
       );
   }
   
-  eventListeners() {
+  initListeners() {
     this.deck
       .elem
       .addEventListener(
         'card-placed',
-        ({ detail: data }) => this.setNewDraggedCard( data )
+        ({ detail: data }) => this.setNewPlayerCard( data )
       );
   }
   
-  setNewDraggedCard( data ) {
+  cardAnimation( cardStyle, shiftX, shiftY, noturn ) {
+    cardStyle.transform = `
+      translate( ${ shiftX }, ${ shiftY } )
+      ${ !noturn ? 'rotateY( -0.5turn )' : '' }
+    `;
+  }
+  
+  setNewPlayerCard( data ) {
     document.querySelector('.hand__player').append( data.card.elem );
     
     const cardStyle = data.card.elem.style;
     
     Object.assign( cardStyle, {
-      left: ( parseInt( data.left ) - this.getRect('.hand__player').left + 1 + 'px' ),
-      top: ( parseInt( data.top ) - this.getRect('.hand__player').top + 1 + 'px' )
+      left: parseInt( data.left ) - this.getRect('.hand__player').left + 1 + 'px',
+      top: parseInt( data.top ) - this.getRect('.hand__player').top + 1 + 'px'
     });
     
-    const shiftX = - parseInt( cardStyle.left, 10 ) + this.playerCardsCount * 60 + 'px' ;
-    const shiftY = - parseInt( cardStyle.top, 10 ) + 'px'
+    const shiftX = - parseInt( cardStyle.left, 10 ) + this.playerCardsCount * 60 + 'px';
+    const shiftY = - parseInt( cardStyle.top, 10 ) + 'px';
     
-    cardStyle.transform = `
-      translateX( ${ shiftX } )
-      translateY( ${ shiftY } )
-      rotateY( -0.5turn )
-    `;
+    this.cardAnimation( cardStyle, shiftX, shiftY, false );
     
     this.playerCardsCount < 7
       ? this.playerCardsCount++
       : this.deck.sub('top').removeEventListener('pointerdown', this.deck.onPointerDown);
   }
   
-  setNewCard() {
+  setNewDealerCard( ) {
     const card = this.deck.topCardData();
     
+    document.querySelector(`.hand__dealer`).append( card.elem );
+    
+    const cardStyle = card.elem.style;
+    const cardStyleRight = this.getRect(`.hand__dealer`).right - this.getRect('[data-zone-deck]').right + 'px';
+    const cardStyleTop = this.getRect(`.hand__dealer`).top - this.getRect('[data-zone-deck]').top + 'px';
+    
+    Object.assign( cardStyle, {
+      left: cardStyleRight,
+      top: cardStyleTop
+    });
+    
+    const shiftX = - parseInt( cardStyleRight, 10 ) + this.dealerCardsCount * 60 + 'px';
+    const shiftY = - parseInt( cardStyleTop, 10 ) + 'px';
+    
+    this.cardAnimation( cardStyle, shiftX, shiftY, this.dealerCardsCount === 0 ? false : true );
+    
+    if ( this.dealerCardsCount < 7 ) this.dealerCardsCount++;
   }
   
   getCardValue( card, currentValue ) {
@@ -76,4 +99,7 @@ export default class Round {
   
   getRect = sel => document.querySelector( sel ).getBoundingClientRect();
   
+  firstAutodraw() {
+    for (let i = 0; i < 2; i++) this.setNewDealerCard();
+  }
 }
