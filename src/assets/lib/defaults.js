5315f0b2fa06ef38a24d70efd6adbbd467463512
Maxim Dejnew <red41tiger@mail.ru>
2021-12-22 15:36:55 +0700

    Minor codestyle fixes

--- src/assets/lib/defaults.js
+++ src/assets/lib/defaults.js
@@ -1,65 +1,54 @@
 export default {
-  hands: function() {
-    return {
-      dealer: {
+  hands: () => ({
+    dealer: {
+      count: 0,
+      value: 0,
+      topaces: 0,
+      overdraft: false,
+      forbiddraw: false
+    },
+    player: {
+      normal: {
         count: 0,
         value: 0,
         topaces: 0,
-        overdraft: false,
-        forbiddraw: false
+        overdraft: false
       },
-      
-      player: {
-        normal: {
-          count: 0,
-          value: 0,
-          topaces: 0,
-          overdraft: false
-        },
-        
-        splitleft: {
-          count: 1,
-          value: 0,
-          topaces: 0,
-          overdraft: false
-        },
-        
-        splitright: {
-          count: 1,
-          value: 0,
-          topaces: 0,
-          overdraft: false
-        }
+      splitleft: {
+        count: 1,
+        value: 0,
+        topaces: 0,
+        overdraft: false
+      },
+      splitright: {
+        count: 1,
+        value: 0,
+        topaces: 0,
+        overdraft: false
       }
     }
-  },
+  }),
+
+  indicators: () => ({
+    player: 3,
+    dealer: 3
+  }),
   
-  indicators: function() {
-    return {
-      player: 3,
-      dealer: 3
+  results: () => ({
+    normal: {
+      player: 0,
+      dealer: 0,
+      tie: 0
+    },
+    left: {
+      player: 0,
+      dealer: 0,
+      tie: 0
+    },
+    right: {
+      player: 0,
+      dealer: 0,
+      tie: 0
     }
-  },
-  
-  results: function() {
-    return {
-      normal: {
-        player: 0,
-        dealer: 0,
-        tie: 0
-      },
-      
-      left: {
-        player: 0,
-        dealer: 0,
-        tie: 0
-      },
-      
-      right: {
-        player: 0,
-        dealer: 0,
-        tie: 0
-      }
-    }
-  }
+  })
 }
