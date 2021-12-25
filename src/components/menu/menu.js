f1f7f4e9f7c6c8427885dfa4ddf37b49084c7253
Maxim Dejnew <red41tiger@mail.ru>
2021-12-25 14:15:12 +0700

    Styles is under refactoring 1: index

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -16,8 +16,8 @@ export default class Menu extends MyComponent {
   
   markup() {
     return `
-      <div class="menu__logo">
-        <img src="src/assets/graphics/logos/logo.png">
+      <div class="menu__logo-wrap">
+        <img class="menu__logo" src="src/assets/graphics/logos/logo.png">
       </div>
       <div class="menu__button-array">
         <div class="menu__button" id="menu-start">START</div>
