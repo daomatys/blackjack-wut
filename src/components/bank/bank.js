ff5ad38ebb7893cd1fec81b0358eb5a7c1759062
Maxim Dejnew <red41tiger@mail.ru>
2021-12-28 10:11:13 +0700

    Bank component is under refactoring: transfered background image to bank selector

--- src/components/bank/bank.js
+++ src/components/bank/bank.js
@@ -11,13 +11,14 @@ export default class Bank extends MyComponent {
   
   markup() {
     return `
-      <img src="src/assets/graphics/bank.png">
       <div class="bank">
-        <div class="bank__slot" id="slot-1"></div>
-        <div class="bank__slot" id="slot-5"></div>
-        <div class="bank__slot" id="slot-10"></div>
-        <div class="bank__slot" id="slot-25"></div>
-        <div class="bank__slot" id="slot-100"></div>
+        <div class="bank__slot-row">
+          <div class="bank__slot" id="slot-1"></div>
+          <div class="bank__slot" id="slot-5"></div>
+          <div class="bank__slot" id="slot-10"></div>
+          <div class="bank__slot" id="slot-25"></div>
+          <div class="bank__slot" id="slot-100"></div>
+        </div>
       </div>
     `;
   }
