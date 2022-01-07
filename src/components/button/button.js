962aee6b521429d359e1185d39dca6bdb92149c9
Maxim Dejnew <red41tiger@mail.ru>
2022-01-07 10:15:16 +0700

    Button component is under construction

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,6 +1,37 @@
 import MyComponent from '../components.js';
 import thatComponentStyleSheet from './button.css' assert { type: 'css' };
 
+
 export default class Button extends MyComponent {
-  
+
+  constructor() {
+    super();
+    this.init();
+  }
+
+  init() {
+    super.applyStyleSheet( thatComponentStyleSheet );
+  }
+
+  changeButtonDisplayState( id ) {
+    const button = document.querySelector(`#${ id }`);
+    
+    const buttonClickIllusion = function switchButtonImagesDisplayState() {
+      button
+        .lastElementChild
+        .classList
+        .toggle('js-img-on_hidden');
+    }
+
+    buttonClickIllusion();
+    
+    document.addEventListener('pointerup', buttonClickIllusion, { once: true } );
+  }
+
+  toggleClickPossibility( suffix ) {
+    const element = document.querySelector(`.clicker-${ suffix }`).lastElementChild;
+    
+    element.classList.toggle('deny-click');
+    element.classList.toggle('allow-click');
+  }
 }
