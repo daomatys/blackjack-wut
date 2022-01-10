dd03de083e84429ce6e842c0eb087cca9ed9ddcc
Maxim Dejnew <red41tiger@mail.ru>
2022-01-10 17:06:25 +0700

    Added new component - modal window

---
+++ src/components/modal-window/modal-window.js
@@ -0,0 +1,35 @@
+import MyComponent from '../components.js';
+import thatComponentStyleSheet from './modal-window.css' assert { type: 'css' };
+
+
+export default class ModalWindow extends MyComponent {
+  
+  constructor( text ) {
+    super();
+
+    this.text = text;
+
+    this.render();
+  }
+  
+  markup() {
+    return `
+      <div class="modal-window">
+        <div class="modal-window__text-window">
+          ${ this.text }
+        </div>
+        <div class="modal-window__close-button">
+        
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    super.initializeComponent({
+      stylesheet: thatComponentStyleSheet,
+      wrapref: '.indicator',
+      markup: this.markup()
+    });
+  }
+}
